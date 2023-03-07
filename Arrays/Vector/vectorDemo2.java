ea0f7381fc1b117f924da47da6a85d86be629145
ahsan <ahan@gmail.com>
2023-03-07 15:53:59 +0500

    vector three cases

---
+++ Arrays/Vector/vectorDemo2.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+public class vectorDemo2 {
+    public static void main(String[] args) {
+        // when we manually provide the Capacity it initiliz the value in 0
+        Vector v1 = new Vector<>(20);
+
+        System.out.println("starting size of vector: " + v1.size());
+        System.out.println("starting Capacity of vector: " + v1.capacity());
+
+        v1.forEach(System.out::println);
+
+    }
+
+}
---
+++ Arrays/Vector/vectorDemo3.java
@@ -0,0 +1,9 @@
+import java.util.Vector;
+
+public class vectorDemo3 {
+    public static void main(String[] args) {
+        Vector v2 = new Vector<>();
+
+    }
+
+}
