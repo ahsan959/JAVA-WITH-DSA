f7535eef8b788cb87e6887e9c84af61c345ad177
Ahsan ali <73709369+ahsan959@users.noreply.github.com>
2023-03-16 00:34:36 +0500

    Two pair sum

---
+++ Arrays/ArrayQuestions/gpc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// This is where the execution of program begins
+int main()
+{
+    // displays Hello World! on screen
+    cout << "Hi C++!";
+
+    return 0;
+}
